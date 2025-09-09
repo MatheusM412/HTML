76993e2e0206c4eb2bb5210c98f5bdac71b72a20
MatheusM412 <cruz.8939@aluno.pr.senac.br>
2025-09-09 16:49:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function btt() {
+	document.getElementById("ppp").innerText = "Faça seu cadrastro antes de continuar";
+	document.getElementById("button").innerHTML = "<button>Faça seu cadrastro aqui</button>";
+
+
+}
+function cadrastro() {
+
+	let dadosUser = prompt("Digite seu nome");
+
+}
